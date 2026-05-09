35c1dff59938a9d930d936b4b7c47e3a1aa0c72c
jota <jotalabota@gmail.com>
2026-05-09 13:38:50 +0300

    cleanup: remove legacy code
    - Delete all original JS source files (replaced by TypeScript)
    - Remove C native addon (ccore, binding.gyp)
    - Remove Go relay, client userscripts
    - Remove old CLI, console, config files

--- scripts/scout-relay/go.mod
+++
@@ -1,9 +0,0 @@
-module scout-relay
-
-go 1.22.0
-
-require (
-	github.com/gorilla/websocket v1.5.3
-	github.com/refraction-networking/utls v1.6.7
-	golang.org/x/net v0.23.0
-)
--- src/ccore/index.js
+++
@@ -1,6 +0,0 @@
-try {
-	module.exports = require("bindings")("ccore");
-} catch (err) {
-	console.error("Ccore is not set up, using JavaScript instead");
-	module.exports = {};
-}
